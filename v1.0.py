ad67fd7f701ddee3828463e7c2338992047e4472
narco <bedo2054@gmail.com>
2024-05-08 23:14:49 +0300

    Fixed 1.0

--- v1.0.py
+++ v1.0.py
@@ -246,53 +246,75 @@ class FileEncryptorDecryptor:
     def affine_decrypt(self, cipher, key):
         return ''.join([chr(((self.ModularInverse(key[0], 26) * (ord(c) - ord('A') - key[1])) % 26) + ord(
             'A')) if c.isalpha() else c for c in cipher])
+
     def generate_matrix(self, key):
-        key = key.replace("j", "i")
-        key = "".join(dict.fromkeys(key))
+        key = key.lower().replace("j", "i")
+        key += "x"  # Ensure 'x' is included in the key
+        key = "".join(dict.fromkeys(key))  # Remove duplicates
         alphabet = "abcdefghiklmnopqrstuvwxyz"
         key += alphabet
         matrix = []
         for char in key:
             if char not in matrix:
                 matrix.append(char)
-        matrix = [matrix[i:i+5] for i in range(0, 25, 5)]
+        matrix = [matrix[i:i + 5] for i in range(0, 25, 5)]
         return matrix
+
     def find_char_position(self, matrix, char):
         for i in range(5):
             for j in range(5):
                 if matrix[i][j] == char:
                     return (i, j)
+        raise ValueError(f"Character {char} not found in matrix")
+
     def playfair_encrypt(self, plain_text, key):
         matrix = self.generate_matrix(key)
         plain_text = plain_text.lower().replace("j", "i")
-        plain_text = plain_text.replace(" ", "")
         cipher_text = ""
-        for i in range(0, len(plain_text), 2):
-            char1 = plain_text[i]
-            char2 = plain_text[i+1] if i+1 < len(plain_text) else 'x'
-            pos1 = self.find_char_position(matrix, char1)
-            pos2 = self.find_char_position(matrix, char2)
-            if pos1[0] == pos2[0]:
-                cipher_text += matrix[pos1[0]][(pos1[1] + 1) % 5] + matrix[pos2[0]][(pos2[1] + 1) % 5]
-            elif pos1[1] == pos2[1]:
-                cipher_text += matrix[(pos1[0] + 1) % 5][pos1[1]] + matrix[(pos2[0] + 1) % 5][pos2[1]]
+        i = 0
+        while i < len(plain_text):
+            if plain_text[i] == " ":
+                cipher_text += " "
+                i += 1
             else:
-                cipher_text += matrix[pos1[0]][pos2[1]] + matrix[pos2[0]][pos1[1]]
+                char1 = plain_text[i]
+                char2 = plain_text[i + 1] if i + 1 < len(plain_text) and plain_text[i + 1] != " " else 'x'
+                try:
+                    pos1 = self.find_char_position(matrix, char1)
+                    pos2 = self.find_char_position(matrix, char2)
+                except ValueError as e:
+                    print(e)
+                    i += 1
+                    continue
+                if pos1[0] == pos2[0]:
+                    cipher_text += matrix[pos1[0]][(pos1[1] + 1) % 5] + matrix[pos2[0]][(pos2[1] + 1) % 5]
+                elif pos1[1] == pos2[1]:
+                    cipher_text += matrix[(pos1[0] + 1) % 5][pos1[1]] + matrix[(pos2[0] + 1) % 5][pos2[1]]
+                else:
+                    cipher_text += matrix[pos1[0]][pos2[1]] + matrix[pos2[0]][pos1[1]]
+                i += 2
         return cipher_text
+
     def playfair_decrypt(self, cipher_text, key):
         matrix = self.generate_matrix(key)
         plain_text = ""
-        for i in range(0, len(cipher_text), 2):
-            char1 = cipher_text[i]
-            char2 = cipher_text[i+1]
-            pos1 = self.find_char_position(matrix, char1)
-            pos2 = self.find_char_position(matrix, char2)
-            if pos1[0] == pos2[0]:
-                plain_text += matrix[pos1[0]][(pos1[1] - 1) % 5] + matrix[pos2[0]][(pos2[1] - 1) % 5]
-            elif pos1[1] == pos2[1]:
-                plain_text += matrix[(pos1[0] - 1) % 5][pos1[1]] + matrix[(pos2[0] - 1) % 5][pos2[1]]
+        i = 0
+        while i < len(cipher_text):
+            if cipher_text[i] == " ":
+                plain_text += " "
+                i += 1
             else:
-                plain_text += matrix[pos1[0]][pos2[1]] + matrix[pos2[0]][pos1[1]]
+                char1 = cipher_text[i]
+                char2 = cipher_text[i + 1]
+                pos1 = self.find_char_position(matrix, char1)
+                pos2 = self.find_char_position(matrix, char2)
+                if pos1[0] == pos2[0]:
+                    plain_text += matrix[pos1[0]][(pos1[1] - 1) % 5] + matrix[pos2[0]][(pos2[1] - 1) % 5]
+                elif pos1[1] == pos2[1]:
+                    plain_text += matrix[(pos1[0] - 1) % 5][pos1[1]] + matrix[(pos2[0] - 1) % 5][pos2[1]]
+                else:
+                    plain_text += matrix[pos1[0]][pos2[1]] + matrix[pos2[0]][pos1[1]]
+                i += 2
         return plain_text
 
 
